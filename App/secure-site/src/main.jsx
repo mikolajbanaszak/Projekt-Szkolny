571fdf62cfe2bde0a67985e25bd6c60ee683239c
gamerberg <mkaminskipoco@gmail.com>
2026-03-17 09:25:42 +0100

    new commit na macku

--- App/secure-site/src/main.jsx
+++ App/secure-site/src/main.jsx
@@ -2,22 +2,28 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./myCss.css";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import UserLoginHours from "./UserLoginHours.jsx";
-import {DataProvider} from "./Json.jsx";
+import { DataProvider } from "./Json.jsx";
+import UserLoginLocations from "./UserLoginLocations.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <DataProvider>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<App />}></Route>
-            <Route
-              path="/user-login-hours/:userId"
-              element={<UserLoginHours />}
-            ></Route>
-          </Routes>
-        </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />}></Route>
+          <Route
+            path="/user-login-hours/:userId"
+            element={<UserLoginHours />}
+          ></Route>
+
+          <Route
+            path="/user-login-locations/:userId"
+            element={<UserLoginLocations />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </DataProvider>
   </StrictMode>,
 );
